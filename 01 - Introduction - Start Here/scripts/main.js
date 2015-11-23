21d8e893c85c40fcf5fe6f9c3f2f477d4b07b08a
Jodi Detch <jodidetch@gmail.com>
2015-11-23 14:34:04 -0500

    create StorePicker component

--- 01 - Introduction - Start Here/scripts/main.js
+++ 01 - Introduction - Start Here/scripts/main.js
@@ -1,1 +1,16 @@
 var React = require('react');
+
+/*
+  StorePicker
+  This will let us make the <StorePicker/> component
+*/
+
+var StorePicker = React.createClass({
+
+  render : function() {
+    return (
+      <p>hello</p>
+    )
+  }
+
+});
