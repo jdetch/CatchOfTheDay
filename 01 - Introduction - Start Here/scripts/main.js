3cbde886a851ca7a70cb2cec02459f22b36429d3
Jodi Detch <jodidetch@gmail.com>
2015-11-24 13:59:52 -0500

    Use History mixin to get store name from input & transition to its page

--- 01 - Introduction - Start Here/scripts/main.js
+++ 01 - Introduction - Start Here/scripts/main.js
@@ -5,6 +5,8 @@ var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var Navigation = ReactRouter.Navigation;
+
+var History = ReactRouter.History;
 var createBrowserHistory = require('history/lib/createBrowserHistory');
 
 var h = require('./helpers');
@@ -76,9 +78,17 @@ var Inventory = React.createClass({
 
 var StorePicker = React.createClass({
 
+  mixins : [History],
+
+  goToStore : function(event) {
+    event.preventDefault();
+    var storeId = this.refs.storeId.value; // get the data from the input
+    this.history.pushState(null, '/store/' + storeId); // transition from <StorePicker/> to <App/> (put it in URL bar)
+  },
+
   render : function() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
         <input type="text" ref="storeId" defaultValue={h.getFunName()} required />
         <input type="Submit"/>
